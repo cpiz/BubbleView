0c89076f920f9da4f4721b4beb4d15bdec05e83f
caijw <caipiz@gmail.com>
2016-05-31 22:24:19 +0800

    修复sample的默认显示case

--- sample/src/main/java/com/cpiz/android/bubblelayoutsample/MainActivity.java
+++ sample/src/main/java/com/cpiz/android/bubblelayoutsample/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_test);
     }
 }
